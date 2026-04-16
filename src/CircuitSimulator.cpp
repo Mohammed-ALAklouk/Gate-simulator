34b9e46880306936889caf5d02c4e45c34bf8106
Mohammed-ALAklouk <moha.alaklouk@gmail.com>
2026-04-16 14:40:55 +0300

    Added grid lines, zoom and panning

--- src/CircuitSimulator.cpp
+++ src/CircuitSimulator.cpp
@@ -3,9 +3,13 @@
 CircuitSimulator::CircuitSimulator()
 {
     SetTraceLogLevel(LOG_ERROR);
-    InitWindow(800, 450, "GateSimulator");
+    InitWindow(window_width, window_height, "GateSimulator");
     SetTargetFPS(60);
     rlImGuiSetup(true);
+
+    panning_context = { 0, 0 };
+    camera.offset = Vector2{ window_width / 2.0f, window_height / 2.0f };
+    camera.zoom = 1;
 }
 
 CircuitSimulator::~CircuitSimulator()
@@ -20,7 +24,51 @@ void CircuitSimulator::HandleInput()
 
 void CircuitSimulator::Update(float deltaTime)
 {
-   
+    switch (current_mouse_state) {
+    case Idle:
+        if (IsMouseButtonDown(MouseButton::MOUSE_BUTTON_LEFT))
+        {
+            current_mouse_state = Panning;
+            panning_context.initial_pos = GetMousePosition();
+            panning_context.initial_target = camera.target;                     
+        }   
+        break;
+    case Panning:
+        {
+            if (IsMouseButtonUp(MouseButton::MOUSE_BUTTON_LEFT)) {
+                current_mouse_state = Idle;
+                break;
+            }
+
+            auto mouse_pos = GetMousePosition();
+        
+            Vector2 delta = {
+                panning_context.initial_pos.x - mouse_pos.x,
+                panning_context.initial_pos.y - mouse_pos.y
+            };
+
+            camera.target = Vector2{
+                panning_context.initial_target.x + (delta.x / camera.zoom),
+                panning_context.initial_target.y + (delta.y / camera.zoom)
+            };
+        }
+
+        break;
+
+        default:
+            break;
+    }
+
+    auto zoom_change = GetMouseWheelMove();
+
+    if (zoom_change)
+    {
+        int index = current_zoom_index + zoom_change;
+        if (index >= 0 && index < 6) {
+            current_zoom_index = index;
+            camera.zoom = zoom_levels[current_zoom_index];
+        }
+    }
 }
 
 void CircuitSimulator::UI()
@@ -32,11 +80,52 @@ void CircuitSimulator::UI()
 void CircuitSimulator::Draw()
 { 
     BeginDrawing();
-    ClearBackground(BLACK);
 
-    
+    BeginMode2D(camera);
+    ClearBackground(darkTheme.background);
+
+    DrawGrid();
+
+    EndMode2D();
+
     UI();
     EndDrawing();
+}
+
+void CircuitSimulator::DrawGrid()
+{
+    Vector2 min = GetScreenToWorld2D({ 0, 0 }, camera);
+    Vector2 max = GetScreenToWorld2D({ (float)GetScreenWidth(), (float)GetScreenHeight() }, camera);
+
+    float startX = floor(min.x / cell_size) * cell_size;
+    float startY = floor(min.y / cell_size) * cell_size;
+    int major_step = 5;
+
+    for (float x = startX; x <= max.x; x += cell_size) {
+        int lineIndex = (int)round(x / cell_size);
+        bool isMajor = (lineIndex % major_step == 0);
+
+        Color color = isMajor ? darkTheme.gridMajor : darkTheme.gridMinor;
+
+        float baseThickness = isMajor ? 1.5f : 0.8f;
+        float adjustedThickness = (baseThickness / camera.zoom);
+        if (adjustedThickness < 1.0f / camera.zoom) adjustedThickness = 1.0f / camera.zoom;
+
+        DrawLineEx({ x, min.y }, { x, max.y }, adjustedThickness, color);
+    }
+    
+    for (float y = startY; y <= max.y; y += cell_size) {
+        int lineIndex = (int)round(y / cell_size);
+        bool isMajor = (lineIndex % major_step == 0);
+
+        Color color = isMajor ? darkTheme.gridMajor : darkTheme.gridMinor;
+
+        float baseThickness = isMajor ? 1.5f : 0.8f;
+        float adjustedThickness = (baseThickness / camera.zoom);
+        if (adjustedThickness < 1.0f / camera.zoom) adjustedThickness = 1.0f / camera.zoom;
+
+        DrawLineEx({ min.x, y }, { max.x, y }, adjustedThickness, color);
+    }
 }
 
 void CircuitSimulator::Run()
--- src/CircuitSimulator.h
+++ src/CircuitSimulator.h
@@ -2,17 +2,58 @@
 #include "raylib.h"
 #include "rlImGui.h"
 #include "imgui.h"
+#include <iostream>
+
+struct PanningContext {
+	Vector2 initial_pos;
+	Vector2 initial_target;
+};
+
+struct UITheme {
+	Color background;
+	Color gridMinor;
+	Color gridMajor;
+	Color logicLow;    // Logic 0
+	Color logicHigh;   // Logic 1
+	Color logicX;      // Undefined
+};
+
+
 
 class CircuitSimulator
 {
 public:
-
 	CircuitSimulator();
 	~CircuitSimulator();
+	void Run();
+	
+private:
 	void HandleInput();
 	void Update(float deltaTime);
 	void UI();
 	void Draw();
 
-	void Run();
+	void DrawGrid();
+	int cell_size = 20;
+	int window_width = 800;
+	int window_height = 450;
+
+	typedef enum { Idle, Panning } MouseState;
+
+	Camera2D camera;
+	const float zoom_levels[6] = { 0.33f, 0.45f, 0.60f, 0.75f, 0.90f, 1.00f };
+	int current_zoom_index = 5; // Start at 1.0 (index 5)
+
+	MouseState current_mouse_state = Idle;
+	PanningContext panning_context;
+
+
+	UITheme darkTheme = {
+	{ 18, 18, 18, 255 },
+	{ 30, 30, 30, 255 },
+	{ 50, 50, 50, 255 },
+	{ 40, 60, 40, 255 },
+	{ 0, 255, 0, 255 },
+	{ 255, 165, 0, 255 }
+	};
 };
